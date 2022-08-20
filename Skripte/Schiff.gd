66bb0e92c4e33e4ed891f819b453c011659d47d4
Reto Russmann <reto.russmann@gmail.com>
2022-08-21 01:49:50 +0200

    Kaputte Schiffsprites eingebaut

--- Skripte/Schiff.gd
+++ Skripte/Schiff.gd
@@ -95,6 +95,8 @@ func todesanimation():
 	if not (s1_gesehen && $"/root/Welt".spieler2_ist_dran == false) && not (s2_gesehen && $"/root/Welt".spieler2_ist_dran):
 		$Todesplayer.play("Tod")
 		$Schiffkaputt.play()
+		if get_parent().name == "EigeneSchiffe":
+			$KaputtSprite.modulate = Color(1, 0.4, 0.4)
 		if Autoload.savegame_data.vibration:
 			Input.vibrate_handheld(70)
 		$"/root/Welt/Camera2D"._screen_shake(0.6, laenge * Autoload.savegame_data.screenshake_value)
